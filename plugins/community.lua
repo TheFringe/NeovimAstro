b0846a9c6acd4dcebe95356d388057bb660641bc
Eric Lundgren <eric.v.lundgren@gmail.com>
2023-05-12 19:40:50 +0200

    Added nord colorscheme, obsidian-nvim and typescript

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,10 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" }
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
